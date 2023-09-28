a174eb1859d9aac5d8c3e90ae49acb0a51cfead4
Divy Patel <39717289+Divy2000@users.noreply.github.com>
2023-09-27 18:45:35 -0700

    Update app_tier_script.py

--- ChatGPTcode/app_tier_script.py
+++ ChatGPTcode/app_tier_script.py
@@ -1,76 +1,65 @@
-# Consolidated Python script for the app tier based on the provided files
+# Combined script for AWS resource setup and app tier execution
 
+# Required imports
 import boto3
 import subprocess
 import json
 
-# Constants (fill these in with your specific details)
+# Constants for AWS setup
 AWS_ACCESS_KEY = 'YOUR_ACCESS_KEY'
 AWS_SECRET_KEY = 'YOUR_SECRET_KEY'
 REGION_NAME = 'us-east-1'
-QUEUE_NAME = "Your_SQS_Queue_Name"
-INPUT_BUCKET = "Your_Input_S3_Bucket"
-OUTPUT_BUCKET = "Your_Output_S3_Bucket"
-IMAGE_CLASSIFICATION_PATH = "/path/to/image_classification.py"  # Adjust this path
+INPUT_BUCKET_NAME = "your-input-bucket-name"
+OUTPUT_BUCKET_NAME = "your-output-bucket-name"
+QUEUE_NAME = "your-sqs-queue-name"
+INSTANCE_TYPE = "t2.micro"  # EC2 instance type, e.g., t2.micro
+AMI_ID = "ami-09c6ef0459a2ff40e"  # ID of the AMI you want to use
+IMAGE_CLASSIFICATION_PATH = "/path/to/image_classification.py"
 LABELS_PATH = "/mnt/data/imagenet-labels.json"  # Path to the JSON file with labels
 
-# Initialize boto3 clients with provided credentials
+# Initialize boto3 clients
 boto3.setup_default_session(aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, region_name=REGION_NAME)
-sqs_client = boto3.client('sqs')
 s3_client = boto3.client('s3')
+sqs_client = boto3.client('sqs')
+ec2_client = boto3.client('ec2')
 cloudwatch_client = boto3.client('cloudwatch')
 
 # Load ImageNet labels
 with open(LABELS_PATH, 'r') as f:
     labels = json.load(f)
 
-
+# Function to process an image
 def process_image(file_name):
-    """Process an image using the deep learning model and return the top prediction."""
     output = subprocess.run(["python3", IMAGE_CLASSIFICATION_PATH, file_name], capture_output=True)
     prediction_index = int(output.stdout.decode().strip())
     return labels[prediction_index]
 
-
+# App Tier function
 def app_tier_handler():
-    """App Tier function to process messages from SQS, run the model, and store results in S3."""
     while True:
-        # Poll the SQS queue for messages
         messages = sqs_client.receive_message(QueueUrl=QUEUE_NAME, MaxNumberOfMessages=10)
-
         if 'Messages' in messages:
             for message in messages['Messages']:
                 image_key = message['Body']
-
-                # Download the image from the S3 input bucket
-                s3_client.download_file(INPUT_BUCKET, image_key, image_key)
-
-                # Process the image using the deep learning model
+                s3_client.download_file(INPUT_BUCKET_NAME, image_key, image_key)
                 result = process_image(image_key)
-
-                # Upload the result to the S3 output bucket
                 output_key = image_key.split('.')[0]
-                s3_client.put_object(Bucket=OUTPUT_BUCKET, Key=output_key, Body=f"{output_key}, {result}")
-
-                # Delete the processed message from the SQS queue
+                s3_client.put_object(Bucket=OUTPUT_BUCKET_NAME, Key=output_key, Body=f"{output_key}, {result}")
                 sqs_client.delete_message(QueueUrl=QUEUE_NAME, ReceiptHandle=message['ReceiptHandle'])
 
-
+# CloudWatch setup function
 def setup_cloudwatch_for_sqs(sqs_queue_arn, threshold):
-    """Setup CloudWatch to monitor the depth of the SQS queue and create an alarm."""
     alarm_name = f"Alarm_{QUEUE_NAME}_Depth"
     metric_name = 'ApproximateNumberOfMessagesVisible'
     namespace = 'AWS/SQS'
     statistic = 'Average'  
     period = 300  
     evaluation_periods = 1  
-
     response = cloudwatch_client.put_metric_alarm(
         AlarmName=alarm_name,
         AlarmDescription=f'Alarm when {QUEUE_NAME} depth exceeds {threshold}',
         ActionsEnabled=True,
         AlarmActions=[
-            # ARN of the Auto Scaling Policy (fill this in)
             'arn:aws:autoscaling:<region>:<account-id>:scalingPolicy:<policy-id>:autoScalingGroupName/<group-name>:policyName/<policy-name>'
         ],
         MetricName=metric_name,
@@ -85,14 +74,40 @@ def setup_cloudwatch_for_sqs(sqs_queue_arn, threshold):
     )
     return response
 
-
-if __name__ == "__main__":
-    # Retrieve the ARN of the SQS queue
-    sqs_queue_arn = sqs_client.get_queue_attributes(QueueUrl=QUEUE_NAME, AttributeNames=['QueueArn'])['Attributes']['QueueArn']
+# AWS resource setup and initiation
+def setup_aws_resources():
+    # Create S3 buckets
+    s3_client.create_bucket(Bucket=INPUT_BUCKET_NAME)
+    s3_client.create_bucket(Bucket=OUTPUT_BUCKET_NAME)
     
-    # Set up CloudWatch monitoring for the SQS queue
+    # Create SQS queue
+    queue_response = sqs_client.create_queue(QueueName=QUEUE_NAME)
+    
+    # Launch EC2 instance
+    ec2_response = ec2_client.run_instances(
+        ImageId=AMI_ID,
+        InstanceType=INSTANCE_TYPE,
+        MinCount=1,
+        MaxCount=1,
+        KeyName="your-key-pair-name"
+    )
+    
+    # Return created resources' details
+    input_bucket_location = s3_client.get_bucket_location(Bucket=INPUT_BUCKET_NAME)['LocationConstraint']
+    output_bucket_location = s3_client.get_bucket_location(Bucket=OUTPUT_BUCKET_NAME)['LocationConstraint']
+    queue_url = queue_response['QueueUrl']
+    instance_id = ec2_response['Instances'][0]['InstanceId']
+    return input_bucket_location, output_bucket_location, queue_url, instance_id
+
+# Main execution
+if __name__ == "__main__":
+    input_loc, output_loc, sqs_url, ec2_id = setup_aws_resources()
+    # If you want to print or log these values, you can do so here
+    # print(input_loc, output_loc, sqs_url, ec2_id)
+    
+    # Set up CloudWatch monitoring
+    sqs_queue_arn = sqs_client.get_queue_attributes(QueueUrl=sqs_url, AttributeNames=['QueueArn'])['Attributes']['QueueArn']
     setup_cloudwatch_for_sqs(sqs_queue_arn, threshold=20)
     
     # Start the app tier handler
     app_tier_handler()
-
